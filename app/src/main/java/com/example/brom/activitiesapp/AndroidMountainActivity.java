21380f48157b745865845d19e0e86aa4c71c7bc5
a18jonsj <a18jonsj@student.his.se>
2019-04-24 09:23:20 +0200

    Lagt till lite på cardet.

--- app/src/main/java/com/example/brom/activitiesapp/AndroidMountainActivity.java
+++ app/src/main/java/com/example/brom/activitiesapp/AndroidMountainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class AndroidMountainActivity extends AppCompatActivity {
@@ -21,5 +22,11 @@ public class AndroidMountainActivity extends AppCompatActivity {
         String incomingBerg = incomingIntent.getStringExtra("Berg");
         Log.d(TAG,"onCreate: Found incoming:" + incomingBerg);
         incomingData.setText(incomingBerg);
+
+        /*ImageView bild1 = findViewById(R.id.bild);
+
+        if (incomingBerg.equals("Mont Blanc")){
+            bild1.setImageResource(R.drawable.denali);
+        } */
     }
 }
